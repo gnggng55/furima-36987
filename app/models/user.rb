3a5ec00172a3bea66fb6c8efa2bcd99e48a796af
gnggng55 <kuk.kwge@gmail.com>
2021-10-24 16:57:38 +0900

    ログイン・ログアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   validates_format_of :password, with: VALID_PASSWORD_REGEX
   validates :nickname, presence: true
-  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "a" }
-  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "b" }
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/}
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
 end
