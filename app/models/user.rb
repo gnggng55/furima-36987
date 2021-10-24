3e3da8ccf1873d247274f159b876aeb1947607c6
gnggng55 <kuk.kwge@gmail.com>
2021-10-24 14:46:19 +0900

    ユーザー新規登録の実装

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+  validates_format_of :password, with: VALID_PASSWORD_REGEX
+  validates :nickname, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "a" }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "b" }
+  validates :birthday, presence: true
+end
