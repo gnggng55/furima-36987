3d8135032c3504ba3981501286a2e3ddb4976dd4
gnggng55 <kuk.kwge@gmail.com>
2021-10-30 12:13:43 +0900

    アクティブハッシュの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,20 @@ class ItemsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+
+  private
+
+    def item_params
+      params.require(:item).permit(:item_name, :delivery_load_id, :prise, :image).merge(user_id: current_user.id)
+    end
+    
 end
