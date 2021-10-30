39ce97b20e7630c90be49fd233d41d5c29e56eee
gnggng55 <kuk.kwge@gmail.com>
2021-10-30 13:52:33 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
   belongs_to :delivery_load
   belongs_to :delivery_area
   belongs_to :delivery_day
+
+  validates :catecory_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :delivery_load_id, numericality: { other_than: 1 }
+  validates :delivery_area_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
+  validates :item_name, :explanation, presence: true
+  validates :price, presence: true, :width, numericality: { less_than_or_equal_to: 300, greater_than_or_equal_to: 9999999 }
 end
