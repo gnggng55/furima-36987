22dd57962ccd7ee00d00b19af0740b3d159a469d
gnggng55 <kuk.kwge@gmail.com>
2021-10-31 17:35:32 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,11 +24,4 @@ class ItemsController < ApplicationController
       params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, 
         :delivery_load_id, :delivery_area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
     end
-
-    def move_to_signed_in
-      unless user_signed_in?
-        redirect_to  user_session_path
-      end
-    end
-
 end
